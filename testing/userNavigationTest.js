0fb46df7d97abc642ac7b7caf660b55eca3c7816
yhuang30 <yhuang30@ncsu.edu>
2018-03-23 16:06:43 -0400

    Added test files and modified login page

---
+++ testing/userNavigationTest.js
@@ -0,0 +1,70 @@
+var config = {
+    url: 'http://sd-vm02.csc.ncsu.edu/#!/',
+};
+
+casper.test.begin('Testing user navigation', function suite(test) {
+    localStorage.clear();
+    test.comment('loading ' + config.url + '...');
+    casper.start(config.url, function() {
+        test.assertTitle("ECO PRT", "ecoPRT title is the same");
+        test.assertUrlMatch(config.url, 'you should be on the home page');
+        
+                  
+    });
+
+    casper.then(function() {
+        this.waitUntilVisible('a', function(){
+        	casper.capture('navigation.png');
+        	test.comment('loading...');
+        	test.comment('clicking on Log In...');
+        	this.clickLabel('Log In', 'a');
+        	test.assertTitle("ECO PRT", "ecoPRT title is the same");
+       	    test.assertUrlMatch(config.url + 'ecoPRTLogin', 'you should be on the Log in page');
+       	    casper.capture('navigation.png');
+        });
+        
+    });
+    
+    casper.then(function() {
+        this.waitUntilVisible('a', function(){
+        	casper.capture('navigation.png');
+        	test.comment('loading...');
+        	test.comment('clicking on Home...');
+        	this.clickLabel('Home', 'a');
+        	test.assertTitle("ECO PRT", "ecoPRT title is the same");
+       	    test.assertUrlMatch(config.url, 'you should be on the Home page');
+       	    casper.capture('navigation.png');
+        });
+        
+    });
+    
+    casper.then(function() {
+        this.waitUntilVisible('a', function(){
+        	casper.capture('navigation.png');
+        	test.comment('loading...');
+        	test.comment('clicking on Sign Up...');
+        	this.clickLabel('Sign Up', 'a');
+        	test.assertTitle("ECO PRT", "ecoPRT title is the same");
+       	    test.assertUrlMatch(config.url + 'ecoPRTSignUp', 'you should be on the SignUp  page');
+       	    casper.capture('navigation.png');
+        });
+        
+    });
+    
+    casper.then(function() {
+        this.waitUntilVisible('a', function(){
+        	casper.capture('navigation.png');
+        	test.comment('loading...');
+        	test.comment('clicking on EcoPRT...');
+        	this.clickLabel('EcoPRT', 'a');
+        	test.assertTitle("ECO PRT", "ecoPRT title is the same");
+       	    test.assertUrlMatch(config.url, 'you should be on the Home page');
+       	    casper.capture('navigation.png');
+        });
+        
+    });
+                  
+    casper.run(function() {
+           test.done();
+    });
+});
