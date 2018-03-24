b4f19f519113db00a08a1e693276c9571aa09836
yhuang30 <yhuang30@ncsu.edu>
2018-03-23 20:20:42 -0400

    modified some of the existing tests

--- testing/adminTest.js
+++ testing/adminTest.js
@@ -5,59 +5,61 @@ var config = {
 casper.test.begin('Testing admin', function suite(test) {
     localStorage.clear();
     test.comment('loading ' + config.url + '...');
+    
     casper.start(config.url, function() {
-        this.waitUntilVisible('a', function() {
+        this.waitForResource(config.url, function() {
+       		casper.capture('1.png');
        		test.assertTitle("ECO PRT", "ecoPRT title is the same");
             test.assertUrlMatch(config.url, 'you should be on the home page');
-            casper.capture('navigation1.png');
         });
-    });
-
-    casper.then(function() {
-        this.waitUntilVisible('a', function(){
-        	casper.capture('navigation.png');
-        	test.comment('loading...');
-        	test.comment('clicking on Log In...');
-        	this.clickLabel('Log In', 'a');
-        	test.assertTitle("ECO PRT", "ecoPRT title is the same");
-       	    test.assertUrlMatch(config.url + 'ecoPRTLogin', 'you should be on the Log in page');
-       	    casper.capture('navigation2.png');
-        });
-        
-    });
-    
-    casper.then(function() {
-        this.wait(1000, function(){
-        	casper.capture('navigation3.png');
-        	this.fillXPath('form#login-form', {
-        		'//input[@id="email"]': 'admin@example.com',
-        		'//input[@id="password"]': 'admin'
-        	}, true);
-        	test.assertUrlMatch(config.url + 'ecoPRTLogin', 'you should be on the Log in page');
-        	this.clickLabel('Sign In', 'button');
-        	casper.capture('navigation4.png');
-        });
-        
     });
 
 	casper.then(function() {
+    	this.waitForResource(config.url, function(){
+    		test.comment('loading...');
+       	    test.comment('clicking on Log In...');
+        	this.clickLabel('Log In', 'a');
+        	test.assertTitle("ECO PRT", "ecoPRT title is the same");
+       		test.assertUrlMatch(config.url + 'ecoPRTLogin', 'you should be on the Log in page');
+       		casper.capture('2.png');
+       	});
+       	
+    });
+
+/*
+    casper.then(function() {
+casper.capture('3.png');
+       	    this.wait(5000, function(){
+        	    casper.capture('4.png');
+          	    this.fillXPath('form#login-form', {
+        		    '//input[@id="email"]': 'admin@example.com',
+        		    '//input[@id="password"]': 'admin'
+        	    }, true);
+        	    test.assertUrlMatch(config.url + 'ecoPRTLogin', 'you should be on the Log in page');
+        	    this.clickLabel('Sign In', 'button');
+        	    casper.capture('5.png');
+            });
+    });*/
+
+
+	casper.then(function() {
         this.wait(1000, function(){
-        	casper.capture('navigation5.png');
+        	casper.capture('admin.png');
         	test.assertUrlMatch(config.url + 'adminHome', 'you should be on the adminHome page');
-        	casper.capture('img.png');
+        	casper.capture('7.png');
         });
         
     });
     
     casper.then(function() {
         this.waitUntilVisible('a', function(){
-        	casper.capture('navigation.png');
+        	casper.capture('home.png');
         	test.comment('loading...');
         	test.comment('clicking on Sign Out...');
         	this.clickLabel('Sign Out', 'a');
         	test.assertTitle("ECO PRT", "ecoPRT title is the same");
        	    test.assertUrlMatch(config.url, 'you should be on the Home page');
-       	    casper.capture('navigation2.png');
+       	    casper.capture('mohe2.png');
         });
         
     });
