0e38a00f39ecee4cd112f61911229c1cc2b238e3
htenoch <htenoch@ncsu.edu>
2018-04-10 10:55:23 -0400

    Update index_v2.js

--- index_v2.js
+++ index_v2.js
@@ -1,7 +1,7 @@
 //Draw every X waypoints
 var edgeResolution = 1;
 
-var runSimulation = true;
+var runSimulation = false;
 var vehiclesRunninginSimulation = 3;
 var distToTravelPerFrame = 1;
 var portToUse = 80;
