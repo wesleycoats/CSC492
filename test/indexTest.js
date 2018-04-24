0e3c72c635e496a334de35523e1b5b5db8a9f8f4
yhuang30 <yhuang30@ncsu.edu>
2018-04-24 15:42:51 -0400

    added more tests. Last successful coverage was: 66.15-statement 63.94-branch 71.74-function 66.23-lines

--- test/indexTest.js
+++ test/indexTest.js
@@ -4,21 +4,14 @@ var chai = require('chai')
 
 chai.use(chaiHttp);
 var expect = chai.expect;
+var fs = require('fs');
 
 describe("REST api Test",function(){
-
-    it("should return home page",function(done){
-        chai.request('http://localhost:80')
-        .get('/')
-        .end(function(err, res) {
-            expect(res).to.have.status(200);
-            done();
-        });
-    });
-    
+    var signupUsername
     describe("Signup Tests",function(){
       it("testing signup form",function(done){
             var emailnum = (Math.random() * 1000)
+            signupUsername = 'testname'+ emailnum + '@test.com'
             chai.request('http://localhost:80')
             .post('/signUp')
             .type('form')
@@ -132,9 +125,10 @@ describe("REST api Test",function(){
         });
     });
     
-    var vehicleID
-    var stationID
-    var stationID2
+    var vehicleID;
+    var stationID;
+    var stationID2;
+    var edgeID;
     
     describe("Adding Tests",function(){
         
@@ -413,10 +407,92 @@ describe("REST api Test",function(){
             });
         });
         
+        it("testing add path",function(done){
+            chai.request('http://localhost:80')
+            .post('/signIn')
+            .type('form')
+            .send({
+                email : 'admin@example.com',
+                password : 'admin'
+            })
+            .end(function(err, res) {
+                var token = res.text
+                var testobj = JSON.parse(fs.readFileSync('test/waypoint.txt'))
+                chai.request('http://localhost:80')
+                .post('/addPath')
+                .set('authToken', token)
+                .send({
+                    startingNode : stationID,
+                    endingNode : stationID2,
+                    distance : 69,
+                    waypoints : testobj
+                })
+                .end(function(err, res) {
+                    //console.log(res)
+                    edgeID = JSON.parse(res.text).id
+                    expect(res).to.have.status(200);
+                    done();
+                });
+            });
+        });
+        
+        it("testing add path bad user",function(done){
+            chai.request('http://localhost:80')
+            .post('/signIn')
+            .type('form')
+            .send({
+                email : 'admi@example.com',
+                password : 'admin'
+            })
+            .end(function(err, res) {
+                var token = res.text
+                var testobj = JSON.parse(fs.readFileSync('test/waypoint.txt'))
+                chai.request('http://localhost:80')
+                .post('/addPath')
+                .set('authToken', token)
+                .send({
+                    startingNode : stationID,
+                    endingNode : stationID2,
+                    distance : 69,
+                    waypoints : testobj
+                })
+                .end(function(err, res) {
+                    //console.log(res)
+                    expect(res).to.have.status(400);
+                    done();
+                });
+            });
+        });
+        
+        it("testing add path missing field",function(done){
+            chai.request('http://localhost:80')
+            .post('/signIn')
+            .type('form')
+            .send({
+                email : 'admin@example.com',
+                password : 'admin'
+            })
+            .end(function(err, res) {
+                var token = res.text
+                var testobj = JSON.parse(fs.readFileSync('test/waypoint.txt'))
+                chai.request('http://localhost:80')
+                .post('/addPath')
+                .set('authToken', token)
+                .send({
+                    startingNode : stationID,
+                    distance : 69,
+                    waypoints : testobj
+                })
+                .end(function(err, res) {
+                    //console.log(res)
+                    expect(res).to.have.status(400);
+                    done();
+                });
+            });
+        });
     });
     
     describe("Editing Tests",function(){
-    
         it("testing edit node",function(done){
             chai.request('http://localhost:80')
             .post('/signIn')
@@ -582,7 +658,89 @@ describe("REST api Test",function(){
                 });
             });
         });
-    
+        
+        it("testing edit path",function(done){
+            chai.request('http://localhost:80')
+            .post('/signIn')
+            .type('form')
+            .send({
+                email : 'admin@example.com',
+                password : 'admin'
+            })
+            .end(function(err, res) {
+                var token = res.text
+                var editsobj = {
+		            distance : "420",
+	            }
+                chai.request('http://localhost:80')
+                .post('/editEdge')
+                .set('authToken', token)
+                .send({
+                    id : edgeID,
+                    edits : editsobj
+                })
+                .end(function(err, res) {
+                    //console.log(res)
+                    expect(res).to.have.status(200);
+                    done();
+                });
+            });
+        });
+        
+        it("testing edit path missing field",function(done){
+            chai.request('http://localhost:80')
+            .post('/signIn')
+            .type('form')
+            .send({
+                email : 'admin@example.com',
+                password : 'admin'
+            })
+            .end(function(err, res) {
+                var token = res.text
+                var editsobj = {
+		            distance : "420",
+	            }
+                chai.request('http://localhost:80')
+                .post('/editEdge')
+                .set('authToken', token)
+                .send({
+                    id : edgeID,
+                })
+                .end(function(err, res) {
+                    //console.log(res)
+                    expect(res).to.have.status(400);
+                    done();
+                });
+            });
+        });
+        
+        it("testing edit path bad account",function(done){
+            chai.request('http://localhost:80')
+            .post('/signIn')
+            .type('form')
+            .send({
+                email : 'admin@example.com',
+                password : 'admi'
+            })
+            .end(function(err, res) {
+                var token = res.text
+                var editsobj = {
+		            distance : "420",
+	            }
+                chai.request('http://localhost:80')
+                .post('/editEdge')
+                .set('authToken', token)
+                .send({
+                    id : edgeID,
+                    edits : editsobj
+                })
+                .end(function(err, res) {
+                    //console.log(res)
+                    expect(res).to.have.status(400);
+                    done();
+                });
+            });
+        });
     });
     
     describe("Deleting Tests",function(){
@@ -705,6 +863,7 @@ describe("REST api Test",function(){
                 });
             });
         });
+        
         it("testing delete vehicle missing field",function(done){
             chai.request('http://localhost:80')
             .post('/signIn')
@@ -753,6 +912,177 @@ describe("REST api Test",function(){
             });
         });
         
+        it("testing delete edge",function(done){
+            chai.request('http://localhost:80')
+            .post('/signIn')
+            .type('form')
+            .send({
+                email : 'admin@example.com',
+                password : 'admin'
+            })
+            .end(function(err, res) {
+                var token = res.text
+                chai.request('http://localhost:80')
+                .post('/deleteEdge')
+                .set('authToken', token)
+                .send({
+                    id: edgeID
+                })
+                .end(function(err, res) {
+                    //console.log(res)
+                    expect(res).to.have.status(200);
+                    done();
+                });
+            });
+        });
         
+        it("testing delete edge missing field",function(done){
+            chai.request('http://localhost:80')
+            .post('/signIn')
+            .type('form')
+            .send({
+                email : 'admin@example.com',
+                password : 'admin'
+            })
+            .end(function(err, res) {
+                var token = res.text
+                chai.request('http://localhost:80')
+                .post('/deleteEdge')
+                .set('authToken', token)
+                .send({
+                    id: ''
+                })
+                .end(function(err, res) {
+                    //console.log(res)
+                    expect(res).to.have.status(400);
+                    done();
+                });
+            });
+        });
+        
+        it("testing delete edge bad account",function(done){
+            chai.request('http://localhost:80')
+            .post('/signIn')
+            .type('form')
+            .send({
+                email : 'admin@example.com',
+                password : 'admi'
+            })
+            .end(function(err, res) {
+                var token = res.text
+                chai.request('http://localhost:80')
+                .post('/deleteEdge')
+                .set('authToken', token)
+                .send({
+                    id: edgeID
+                })
+                .end(function(err, res) {
+                    //console.log(res)
+                    expect(res).to.have.status(400);
+                    done();
+                });
+            });
+        });
+        
+    });
+    
+    describe("Misc Tests",function(){
+        it("should return home page",function(done){
+            chai.request('http://localhost:80')
+            .get('/')
+            .end(function(err, res) {
+                expect(res).to.have.status(200);
+                done();
+            });
+        });
+        
+        it("testing user info: must be signed in",function(done){
+            chai.request('http://localhost:80')
+            .get('/userInfo')
+            .end(function(err, res) {
+                expect(res).to.have.status(400);
+                done();
+            });
+        });
+        
+        it("testing user info: admin",function(done){
+            chai.request('http://localhost:80')
+            .post('/signIn')
+            .type('form')
+            .send({
+                email : 'admin@example.com',
+                password : 'admin'
+            })
+            .end(function(err, res) {
+                var token = res.text
+                chai.request('http://localhost:80')
+                .get('/userInfo')
+                .set('authToken', token)
+                .end(function(err, res) {
+                    expect(res).to.have.status(200);
+                    done();
+                });
+            });
+        });
+        
+        it("testing user info: admin",function(done){
+            chai.request('http://localhost:80')
+            .post('/signIn')
+            .type('form')
+            .send({
+                email : 'admin@example.com',
+                password : 'admin'
+            })
+            .end(function(err, res) {
+                var token = res.text
+                chai.request('http://localhost:80')
+                .get('/userInfo')
+                .set('authToken', token)
+                .end(function(err, res) {
+                    expect(res).to.have.status(200);
+                    done();
+                });
+            });
+        });
+        
+        it("testing user info: admin - bad token",function(done){
+            chai.request('http://localhost:80')
+            .post('/signIn')
+            .type('form')
+            .send({
+                email : 'admin@example.com',
+                password : 'admin'
+            })
+            .end(function(err, res) {
+                var token = 'bad'
+                chai.request('http://localhost:80')
+                .get('/userInfo')
+                .set('authToken', token)
+                .end(function(err, res) {
+                    expect(res).to.have.status(400);
+                    done();
+                });
+            });
+        });
+        
+        it("testing user info: rider",function(done){
+            chai.request('http://localhost:80')
+            .post('/signIn')
+            .type('form')
+            .send({
+                email : signupUsername,
+                password : 'password'
+            })
+            .end(function(err, res) {
+                var token = res.text
+                chai.request('http://localhost:80')
+                .get('/userInfo')
+                .set('authToken', token)
+                .end(function(err, res) {
+                    expect(res).to.have.status(200);
+                    done();
+                });
+            });
+        });
     });
 });
